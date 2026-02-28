f775e9ef7adffcac6ab801248a86a73be920d6d3
alwolf <y_alang@outlook.com>
2026-02-28 11:06:55 +0800

    fix: 更新Windows安装包下载链接的文件名
    修复GitHub发布页中Windows安装包的文件名，从"Minecraft.-Windows-0.0.1-Setup.exe"更正为"Windows-0.0.1-Setup-x64.exe"，以匹配实际发布的文件。

--- lib/config.ts
+++ lib/config.ts
@@ -8,7 +8,7 @@ export const WebConfig = {
         gitee: "https://gitee.com/al01/mcsd"
     },
     downloadUrl: {
-        github: "https://github.com/al01cn/mcsd/releases/download/app/Minecraft.-Windows-0.0.1-Setup.exe",
+        github: "https://github.com/al01cn/mcsd/releases/download/app/Windows-0.0.1-Setup-x64.exe",
         gitee: "https://gitee.com/al01/mcsd/releases/download/v0.0.1/Minecraft%20%E9%9F%B3%E9%A2%91%E5%8C%85%E7%94%9F%E6%88%90%E5%99%A8-Windows-0.0.1-Setup.exe"
     },
     qq: {
